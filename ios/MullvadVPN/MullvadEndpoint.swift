c79bdc56f9c800f4e83ada96a4f70eae417f7010
Andrej Mihajlov <and@mullvad.net>
2019-06-12 13:38:38 +0200

    Add MullvadEndpoint

---
+++ ios/MullvadVPN/MullvadEndpoint.swift
@@ -0,0 +1,18 @@
+//
+//  MullvadEndpoint.swift
+//  MullvadVPN
+//
+//  Created by pronebird on 12/06/2019.
+//  Copyright © 2019 Amagicom AB. All rights reserved.
+//
+
+import Foundation
+import Network
+
+/// Contains server data needed to connect to a single mullvad endpoint
+struct MullvadEndpoint {
+    let endpoint: NWEndpoint
+    let ipv4Gateway: IPv4Address
+    let ipv6Gateway: IPv6Address
+    let publicKey: String
+}
